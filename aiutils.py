a2ec4ec0c8e70189d82189e438c190ecc04e59ce
Hasan uz zaman Sheikh <hasan@live.in>
2023-07-18 21:39:50 +0530

    modification to load txt only with TextLoader and end point to initdata

--- aiutils.py
+++ aiutils.py
@@ -5,14 +5,18 @@ from langchain import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import DirectoryLoader, TextLoader
 from decouple import config
-
+import sys
+import os
 
 openai_api_key = config("OPENAI_API_KEY")
 
 
 # it loads a directory of documents and return vector db
 def load_documents():
-    loader = DirectoryLoader('data/', loader_cls=TextLoader)
+    if not os.path.exists('data/'):
+        print('data folder does not exist')
+        return None
+    loader = DirectoryLoader('data/', glob='**/*.txt', loader_cls=TextLoader)
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     return text_splitter.split_documents(documents)  
@@ -20,6 +24,9 @@ def load_documents():
 def get_qa():
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
     texts = load_documents()
+    if texts is None:
+        print('texts is none possibly due to data folder does not exist')
+        return None
     docsearch = FAISS.from_documents(texts, embeddings)
     llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
     # Create your Retriever
@@ -28,14 +35,17 @@ def get_qa():
 
 def query_my_question(queryText):
     qa=get_qa()
+    if qa is None:
+        print('qa is none possibly due to data folder does not exist')
+        return 'unable to answer your question'
     query={"query": queryText}
     result=qa(query)
     return result['result']
 
-   
 # Compare this snippet from app.py:
 if __name__ == '__main__':
-    qa=get_qa()
-    query={"query": "where am i today?"}
-    result=qa(query)
-    print(result['result'])
+    if len(sys.argv) <2 :
+        print('not enough arguments')
+        sys.exit(1)
+    print(f'querying {sys.argv[1]}')
+    print(query_my_question(sys.argv[1]))
--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from decouple import config
 import openai
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
+from aiutils import query_my_question, get_qa
+from azureblobutil import download_blobs
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for )
 
 app = Flask(__name__)
 openai.api_key = config("OPENAI_API_KEY")
+
 
 @app.route('/message', methods=['POST'])
 def message():
@@ -17,44 +20,22 @@ def message():
     print(f"Chatting with this number: {whatsapp_number}")
     resp = MessagingResponse()
     msg = resp.message()
-    responded = False
-    if 'quote' in incoming_msg:
-        # return a quote
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
-            quote = f'{data["content"]} ({data["author"]})'
-        else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
-        msg.body(quote)
-        responded = True
-    if 'cat' in incoming_msg:
-        # return a cat pic
-        msg.media('https://cataas.com/cat')
-        responded = True
-        
-       # Call the OpenAI API to generate text with ChatGPT
-    messages = [{"role": "user", "content": incoming_msg}]
-    messages.append({"role": "system", "content": "You're an English teacher who has taught 100s of students grammar, idioms, vocab, basic English information, and beyond basics."})
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        max_tokens=200,
-        n=1,
-        stop=None,
-        temperature=0.5
-    )
-    msg.body(response.choices[0].message.content)
-    responded = True
-    
-    if not responded:
-        msg.body('I only know about famous quotes and cats, sorry!')
+    # Call the OpenAI API to generate text with ChatGPT
+    answer=query_my_question(incoming_msg)
+    msg.body(answer)
     return str(resp)
 
+@app.route('/initdata', methods=['GET'])
+def initdata():
+    try:
+        download_blobs()
+        return "ok"
+    except Exception as e:
+        return "error initializing data"
   
 @app.route('/health', methods=['GET'])
 def health():
     return "ok"
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
